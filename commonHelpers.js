f0d2e0e1e4dc32e96ebc406c683c9095fbf3accc
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-01-13 09:00:20 +0000

    Deploying to gh-pages from @ YuliiaLukomska/goit-js-hw-10@f3c0c2afa64f63e24fa037d89da7c96249d0bf4a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/reset-a3b7b90b.js";import{f as i}from"./assets/vendor-992cd48f.js";i("#datetime-picker",{});
+import"./assets/reset-a3b7b90b.js";import{f as r}from"./assets/vendor-992cd48f.js";let a;const e=document.querySelector("button[data-start]");e.disabled=!0;r("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){console.log(t[0]),a=t[0],a-new Date<0?(e.disabled=!0,alert("Please choose a date in the future")):e.disabled=!1}});function n(){setInterval(()=>{},1e3)}e.addEventListener("click",n);
 //# sourceMappingURL=commonHelpers.js.map
