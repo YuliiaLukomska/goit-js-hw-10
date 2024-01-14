86270c6f5c1cee7dd2606aee9bcfe58ac62c6b27
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-01-14 19:57:04 +0200

    task-2 is finished

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formRef = document.querySelector('form');
+formRef.addEventListener('submit', event => {
+  onCreatePromise(event)
+    .then(value =>
+      iziToast.success({
+        title: 'OK',
+        message: `Fulfilled promise in ${value}ms`,
+        position: 'topRight',
+        titleColor: 'white',
+        messageColor: 'white',
+        timeout: false,
+        backgroundColor: '#59A10D',
+      })
+    )
+    .catch(error =>
+      iziToast.error({
+        message: `Rejected promise in ${error}ms`,
+        position: 'topRight',
+        iconUrl: '../img/left-close.svg',
+        titleColor: 'white',
+        messageColor: 'white',
+        timeout: false,
+        backgroundColor: '#EF4040',
+      })
+    );
+});
+
+function onCreatePromise(event) {
+  event.preventDefault();
+  const delay = event.currentTarget.elements.delay.value;
+  const state = event.currentTarget.elements.state.value;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
